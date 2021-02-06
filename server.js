a4a2e6b2fa7fc5595c23522a7b8dd16d74b9f196
coffmanjrp <coffmanjrp@gmail.com>
2021-02-05 16:04:38 -0800

    implement server side json

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ require('dotenv').config;
 
 const app = express();
 
+app.get('/api/customers', (req, res) => {
+  const customers = [
+    {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe',
+    },
+    {
+      id: 2,
+      firstName: 'Carl',
+      lastName: 'Smith',
+    },
+    {
+      id: 3,
+      firstName: 'Paul',
+      lastName: 'Jr.',
+    },
+  ];
+
+  res.json(customers);
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`.blue.bold));
